f722da9626b4c443d55910cae9af8d7216e4e631
Darkhii1 <jul.auvinet@gmail.com>
2025-12-13 18:55:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+
+    if (menuToggle) {
+        menuToggle.addEventListener('click', () => {
+            nav.classList.toggle('active');
+        });
+    }
+});
